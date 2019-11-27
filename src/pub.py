92db3c7aabedebdf15fd0a14679e6ed78876c2d2
leonardo lima <leo.lima21051990@gmail.com>
2019-11-27 15:30:04 -0300

    Add a metods and a main function

--- src/pub.py
+++ src/pub.py
@@ -15,14 +15,77 @@ class ninja_turtle:
     self.velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
     # Subscriber for the topic '/turtle1/pose'
     self.pose_subscriber = rospy.Subscriber('/turtle1/pose', Pose, self.update_pose)
-
+    # Pose var using a Pose function from turtlesim.msg
     self.pose = Pose()
+    # Setup the loop rate to 10hz
     self.rate = rospy.Rate(10)
   
+  # Callback function. called when a new msg is received by the subscriber
+  def update_pose(self, data):
+    self.pose = data
+    self.pose.x = round(self.pose.x, 4)
+    self.pose.y = round(self.pose.y, 4)
+
+  # Euclidean distance between current pose and the goal pose
+  def euclidean_distance(self, goal_pose):
+    return sqrt(pow((goal_pose.x - self.pose.x), 2) + pow((goal_pose.y - self.pose.y), 2))
   
+  # Set the linear vel
+  def linear_vel(self, goal_pose, constant=1.5):
+    return constant * self.euclidean_distance(goal_pose)
 
+  # Set the angle
+  def steering_angle(self, goal_pose):
+    return atan2(goal_pose.y - self.pose.y, goal_pose.x - self.pose.x)
 
+  # Set the angular vel
+  def angular_vel(self, goal_pose, constant=6):
+    return constant * (self.steering_angle(goal_pose) - self.pose.theta)
 
+  # Move the turtle to the goal point
+  def move2goal(self):
+    goal_pose = Pose()
+
+    # Get the input from the user.
+    goal_pose.x = input("Set your x goal: ")
+    goal_pose.y = input("Set your y goal: ")
+
+    # Please, insert a number slightly greater than 0 (e.g. 0.01).
+    distance_error = 0.1
+    vel_msg = Twist()
+
+    # Main loop move2goal function
+    while self.euclidean_distance(goal_pose) >= distance_error:
+      vel_msg.linear.x = self.linear_vel(goal_pose)
+      vel_msg.linear.y = 0
+      vel_msg.linear.z = 0
+
+      # Angular velocity in the z-axis.
+      vel_msg.angular.x = 0
+      vel_msg.angular.y = 0
+      vel_msg.angular.z = self.angular_vel(goal_pose)
+
+      # Publishing our vel_msg
+      self.velocity_publisher.publish(vel_msg)
+
+      # Publish at the desired rate.
+      self.rate.sleep()
+
+    # Stopping our robot after the movement is over.
+    vel_msg.linear.x = 0
+    vel_msg.angular.z = 0
+    self.velocity_publisher.publish(vel_msg)
+
+    # If we press control + C, the node will stop.
+    rospy.spin()
+
+# Main function
+if __name__ == "__main__":
+  try:
+    leonardo = ninja_turtle()
+    leonardo.move2goal
+  except rospy.ROSInterruptException:
+    pass  
 
 
 
