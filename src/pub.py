7505ab7b6ab3577878de1aeb2dc519465f48c4bb
leonardo lima <leo.lima21051990@gmail.com>
2019-11-27 14:10:09 -0300

    Add a turtlebot class to the code

--- src/pub.py
+++ src/pub.py
@@ -1,9 +1,28 @@
 #!/usr/bin/env python
-#libs:
+
+# libs:
 import rospy
 from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
 from math import pow, atan2, sqrt
+
+# This class contain all the aspects of robot, like a nodes and metods
+class ninja_turtle:
+  def __init__(self):
+    # Creates a node 
+    rospy.init_node('ninjaturtle_controller', anonymous=True)
+    # Publisher for the topic '/turtle1/cmd_vel'
+    self.velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
+    # Subscriber for the topic '/turtle1/pose'
+    self.pose_subscriber = rospy.Subscriber('/turtle1/pose', Pose, self.update_pose)
+
+    self.pose = Pose()
+    self.rate = rospy.Rate(10)
+  
+  
+
+
+
 
 
 
